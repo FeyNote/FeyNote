622635d314b1eabce801ad7ffee8e07611062236
julianpoyourow <julian@poyourow.com>
2024-03-24 22:28:32 +0000

    feat: tiptap & tiptap pro

--- apps/frontend/src/app/components/tiptap/TiptapEditor.tsx
+++ apps/frontend/src/app/components/tiptap/TiptapEditor.tsx
@@ -4,6 +4,7 @@ import ListItem from '@tiptap/extension-list-item';
 import { TiptapMenuBar } from './TiptapMenuBar';
 import { IonCard } from '@ionic/react';
 import styled from 'styled-components';
+import UniqueID from '@tiptap-pro/extension-unique-id';
 
 const StyledEditorContent = styled(EditorContent)`
   .tiptap {
@@ -19,7 +20,13 @@ interface Props {
 
 export const TiptapEditor = (props: Props) => {
   const editor = useEditor({
-    extensions: [StarterKit, ListItem],
+    extensions: [
+      StarterKit,
+      ListItem,
+      UniqueID.configure({
+        types: ['heading', 'paragraph'],
+      }),
+    ],
     content: props.content,
     onUpdate: ({ editor }) => {
       props.onContentChange?.(editor.getHTML());
