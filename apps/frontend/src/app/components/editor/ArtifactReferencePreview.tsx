3977ed69f06e6334d9fdde1f0d911c225f250678
julianpoyourow <julian@poyourow.com>
2024-05-21 00:59:24 +0000

    fix: popup now always positions itself within view

--- apps/frontend/src/app/components/editor/ArtifactReferencePreview.tsx
+++ apps/frontend/src/app/components/editor/ArtifactReferencePreview.tsx
@@ -10,17 +10,26 @@ import { useTranslation } from 'react-i18next';
 import { useMemo } from 'react';
 import { createPortal } from 'react-dom';
 
+const PREVIEW_WIDTH_PX = 400;
+const PREVIEW_MIN_HEIGHT_PX = 100;
+const PREVIEW_MAX_HEIGHT_PX = 300;
+
 const Container = styled.div<{
-  $top: number;
-  $left: number;
+  $top?: number;
+  $left?: number;
+  $right?: number;
+  $bottom?: number;
 }>`
   position: absolute;
-  top: ${(props) => props.$top}px;
-  left: ${(props) => props.$left}px;
+  ${(props) => props.$top !== undefined && `top: ${props.$top}px;`}
+  ${(props) => props.$left !== undefined && `left: ${props.$left}px;`}
+  ${(props) => props.$right !== undefined && `right: ${props.$right}px;`}
+  ${(props) => props.$bottom !== undefined && `top: ${props.$bottom}px;`}
+  ${(props) => props.$bottom !== undefined && `transform: translateY(-100%);`}
   z-index: 100;
-  width: 400px;
-  min-height: 100px;
-  max-height: 300px;
+  width: ${PREVIEW_WIDTH_PX}px;
+  min-height: ${PREVIEW_MIN_HEIGHT_PX}px;
+  max-height: ${PREVIEW_MAX_HEIGHT_PX}px;
   overflow-y: auto;
   background: var(--ion-background-color);
   box-shadow: 1px 1px 7px rgba(0, 0, 0, 0.3);
@@ -64,20 +73,43 @@ export const ArtifactReferencePreview: React.FC<Props> = (props) => {
     initialContent,
   });
 
-  const top = useMemo(
-    () =>
-      props.previewTarget.getBoundingClientRect().top +
-      props.previewTarget.getBoundingClientRect().height,
-    [props.previewTarget],
-  );
-  const left = useMemo(
-    () => props.previewTarget.getBoundingClientRect().left,
-    [props.previewTarget],
-  );
+  const bounds = useMemo(() => {
+    const previewTargetBoundingRect =
+      props.previewTarget.getBoundingClientRect();
+    const bounds = {} as {
+      top?: number;
+      bottom?: number;
+      left?: number;
+      right?: number;
+    };
+
+    if (
+      previewTargetBoundingRect.top +
+        previewTargetBoundingRect.height +
+        PREVIEW_MAX_HEIGHT_PX <
+      window.innerHeight
+    ) {
+      bounds.top =
+        previewTargetBoundingRect.top + previewTargetBoundingRect.height;
+    } else {
+      bounds.bottom = previewTargetBoundingRect.top;
+    }
+    if (previewTargetBoundingRect.left + PREVIEW_WIDTH_PX < window.innerWidth) {
+      bounds.left = previewTargetBoundingRect.left;
+    } else {
+      bounds.right = 0;
+    }
+    return bounds;
+  }, [props.previewTarget]);
 
   // We portal because blocknote styling does not play well with blocknote instances inside of each other
   return createPortal(
-    <Container $top={top} $left={left}>
+    <Container
+      $top={bounds.top}
+      $left={bounds.left}
+      $bottom={bounds.bottom}
+      $right={bounds.right}
+    >
       <Header>{props.artifact.title}</Header>
       {initialContent && props.artifact.text.trim().length ? (
         <BlockNoteView editor={editor} editable={false}></BlockNoteView>
