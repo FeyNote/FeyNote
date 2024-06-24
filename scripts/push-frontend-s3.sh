d689da5df113c66b83e0b3b6bbafcfe7100626a0
julianpoyourow <julian@poyourow.com>
2024-06-24 05:30:42 +0000

    fix: do not flag acl when pushing assets, cleanup script comments

--- scripts/push-frontend-s3.sh
+++ scripts/push-frontend-s3.sh
@@ -12,27 +12,6 @@ then
   exit 1
 fi
 
-# mkdir -p www-revhashed
-#
-# find www/ -regextype egrep -regex '.+\-[a-zA-Z0-9]{8}\..+' -exec mv -t www-revhashed/ -- {} +
-
-# Time sensitive assets only
 aws s3 sync www s3://feynote-www/frontend/$TAG/ \
-  # --exclude "*" \
-  # --include "index.html" \
-  # --include "service-worker.js" \
-  --acl public-read \
   --cache-control "public, max-age=3600, must-revalidate"
-
-# General, non-revhashed frontend files - semi time sensitive
-# aws s3 sync www s3://feynote-www/frontend/$TAG/ \
-#   --exclude "index.html" \
-#   --exclude "service-worker.js" \
-#   --acl public-read \
-#   --cache-control "public, max-age=604800, must-revalidate"
-
-# Revhashed files (previously copied to www-revhashed) - persist long-term
-# aws s3 sync www-revhashed s3://feynote-www/frontend/$TAG/ \
-#   --acl public-read \
-#   --cache-control "public, max-age=2592000, immutable"
 
