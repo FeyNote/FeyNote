6abe18cbe1190c9d4f3bfad996232697847dc43b
julianpoyourow <julian@poyourow.com>
2024-06-25 03:55:23 +0000

    fix: lint

--- apps/frontend/src/app/components/editor/artifactThemeTitleI18nByName.ts
+++ apps/frontend/src/app/components/editor/artifactThemeTitleI18nByName.ts
@@ -1,10 +1,5 @@
 import type { ArtifactTheme } from '@prisma/client';
 
-// export const ArtifactTheme = {
-//   default: "default",
-//   classic: "classic",
-// } as const;
-//
 export const artifactThemeTitleI18nByName = {
   default: 'artifactTheme.default',
   classic: 'artifactTheme.classic',
