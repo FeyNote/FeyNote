f4069b6c91dfca16d5c7dc0ebd5e8123ec6fa289
julianpoyourow <julian@poyourow.com>
2024-06-25 22:26:34 +0000

    fix: cors allowlist

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,35 @@
 import express from 'express';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter, createContext } from '@feynote/trpc';
+import cors from 'cors';
 
 const app = express();
+
+const defaultCorsAllowlist = [
+  'https://feynote.com',
+  'https://beta.feynote.com',
+  'https://staging.feynote.com',
+];
+
+const hostMatch = (pattern: string, origin: string) => {
+  if (pattern.endsWith('*')) {
+    return origin.startsWith(pattern.substring(0, pattern.length - 1));
+  }
+
+  return origin === pattern;
+};
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowCors =
+      origin &&
+      defaultCorsAllowlist.some((pattern) => hostMatch(pattern, origin));
+    callback(null, allowCors);
+  },
+} satisfies cors.CorsOptions;
+
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(
   '/trpc',
