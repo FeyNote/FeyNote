ab659c78132a613fe5af10a67b052132443c419c
cmeyer <chrismeyer175@gmail.com>
2024-06-28 03:23:28 +0000

    Threads and Messages table migration

--- prisma/migrations/20240628030309_messages_and_threads_table/migration.sql
+++ prisma/migrations/20240628030309_messages_and_threads_table/migration.sql
@@ -1,3 +1,9 @@
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "File" ALTER COLUMN "userId" DROP NOT NULL;
+
 -- CreateTable
 CREATE TABLE "Thread" (
     "id" UUID NOT NULL,
@@ -22,6 +28,9 @@ CREATE TABLE "Message" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Message_createdAt_key" ON "Message"("createdAt");
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Thread" ADD CONSTRAINT "Thread_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
