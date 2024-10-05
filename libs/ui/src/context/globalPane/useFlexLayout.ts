c0dcb935fae9c2564533eed956f943cccdaf9cef
julianpoyourow <julian@poyourow.com>
2024-10-05 21:57:58 +0000

    feat: reset global pane layout on auth

---
+++ libs/ui/src/context/globalPane/useFlexLayout.ts
@@ -0,0 +1,89 @@
+import { useMemo, useReducer, useRef } from 'react';
+import { IGlobalAttributes, Model } from 'flexlayout-react';
+import { PaneableComponent } from './PaneableComponent';
+import { t } from 'i18next';
+
+export const SAVED_LAYOUT_LOCAL_STORAGE_KEY = 'savedLayout';
+
+const defaultGlobalLayoutAttributes = {
+  tabEnableRename: false,
+  tabEnableFloat: false,
+  tabDragSpeed: 0.2,
+  tabSetMinWidth: 200,
+  tabSetEnableMaximize: false,
+  tabSetTabStripHeight: 36,
+} satisfies IGlobalAttributes;
+
+function getDefaultLayout() {
+  return Model.fromJson({
+    global: {
+      ...defaultGlobalLayoutAttributes,
+    },
+    borders: [],
+    layout: {
+      type: 'row',
+      weight: 100,
+      children: [
+        {
+          type: 'tabset',
+          weight: 50,
+          children: [
+            {
+              id: 'default',
+              type: 'tab',
+              name: t('dashboard.title'),
+              config: {
+                component: PaneableComponent.Dashboard,
+                props: {},
+                navigationEventId: crypto.randomUUID(),
+              },
+            },
+          ],
+        },
+      ],
+    },
+  });
+}
+
+export const useFlexLayout = () => {
+  const [_, triggerRerender] = useReducer((x) => x + 1, 0);
+
+  const savedLayout = useMemo(() => {
+    const savedLayoutStr = localStorage.getItem(SAVED_LAYOUT_LOCAL_STORAGE_KEY);
+
+    if (savedLayoutStr) {
+      const json = JSON.parse(savedLayoutStr);
+      return Model.fromJson(json);
+    } else {
+      return getDefaultLayout();
+    }
+  }, []);
+
+  const layoutRef = useRef<Model>(savedLayout);
+
+  const saveLayout = (): void => {
+    localStorage.setItem(
+      SAVED_LAYOUT_LOCAL_STORAGE_KEY,
+      JSON.stringify(layoutRef.current.toJson()),
+    );
+  };
+
+  const applyLayoutJson = (json: any) => {
+    layoutRef.current = Model.fromJson(json);
+    saveLayout();
+    triggerRerender();
+  };
+
+  const resetLayout = (): void => {
+    layoutRef.current = getDefaultLayout();
+    saveLayout();
+    triggerRerender();
+  };
+
+  return {
+    layout: layoutRef.current,
+    applyLayoutJson,
+    resetLayout,
+    saveLayout,
+  };
+};
--- libs/ui/src/context/session/SessionContextProviderWrapper.tsx
+++ libs/ui/src/context/session/SessionContextProviderWrapper.tsx
@@ -1,8 +1,9 @@
-import { ReactNode, useEffect, useMemo, useState } from 'react';
+import { ReactNode, useContext, useEffect, useMemo, useState } from 'react';
 import { SessionContext, type SessionContextData } from './SessionContext';
 import { appIdbStorageManager } from '../../utils/AppIdbStorageManager';
 import type { SessionDTO } from '@feynote/shared-utils';
 import { Auth } from '../../components/auth/Auth';
+import { GlobalPaneContext } from '../globalPane/GlobalPaneContext';
 
 interface Props {
   children: ReactNode;
@@ -11,16 +12,20 @@ interface Props {
 export const SessionContextProviderWrapper = ({
   children,
 }: Props): JSX.Element => {
+  const [loading, setLoading] = useState(true);
   const [session, setSession] = useState<SessionDTO | null>(null);
+  const { resetLayout } = useContext(GlobalPaneContext);
 
   useEffect(() => {
     appIdbStorageManager.getSession().then((session) => {
       setSession(session);
+      setLoading(false);
     });
   }, []);
 
   const setAndPersistSession = async (newSession: SessionDTO | null) => {
     await appIdbStorageManager.deleteAllData();
+    if (resetLayout) resetLayout();
 
     if (newSession) {
       await appIdbStorageManager.setSession(newSession);
@@ -37,6 +42,11 @@ export const SessionContextProviderWrapper = ({
     }),
     [session],
   );
+
+  // We wait for the async idb call to finish so we don't flash the register page
+  if (loading) {
+    return <></>;
+  }
 
   if (!value.session) {
     return (
