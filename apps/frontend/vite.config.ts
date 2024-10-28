485bf3bf7ca56ff99c050d9461e230b99cc0cf41
julianpoyourow <julian@poyourow.com>
2024-10-28 17:13:03 +0000

    chore: remove excalidraw IS_PREACT

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -27,10 +27,6 @@ export default defineConfig({
     host: '0.0.0.0',
   },
 
-  define: {
-    'process.env.IS_PREACT': JSON.stringify('false'),
-  },
-
   plugins: [
     react(),
     nxViteTsPaths(),
