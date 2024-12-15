01608df1e879f050a214d58c250d6d05cd5cd3fd
cmeyer <chrismeyer175@gmail.com>
2024-12-15 22:40:12 +0000

    fixing import jobs migration

--- prisma/migrations/20241215223954_import_jobs/migration.sql
+++ prisma/migrations/20241215223954_import_jobs/migration.sql
@@ -4,14 +4,17 @@ CREATE TYPE "ImportJobType" AS ENUM ('Logseq', 'Obsidian', 'Notion', 'GoogleDriv
 -- CreateEnum
 CREATE TYPE "JobStatus" AS ENUM ('Error', 'Success', 'InProgress');
 
+-- AlterEnum
+ALTER TYPE "FilePurpose" ADD VALUE 'import';
+
 -- CreateTable
 CREATE TABLE "ImportJob" (
     "id" UUID NOT NULL,
     "title" TEXT NOT NULL,
     "userId" UUID NOT NULL,
+    "fileId" UUID,
     "type" "ImportJobType" NOT NULL,
     "status" "JobStatus" NOT NULL,
-    "s3" TEXT NOT NULL,
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ(6) NOT NULL,
 
@@ -19,7 +22,13 @@ CREATE TABLE "ImportJob" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "ImportJob_fileId_key" ON "ImportJob"("fileId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "ImportJob_createdAt_key" ON "ImportJob"("createdAt");
 
 -- AddForeignKey
 ALTER TABLE "ImportJob" ADD CONSTRAINT "ImportJob_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ImportJob" ADD CONSTRAINT "ImportJob_fileId_fkey" FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE SET NULL ON UPDATE CASCADE;
