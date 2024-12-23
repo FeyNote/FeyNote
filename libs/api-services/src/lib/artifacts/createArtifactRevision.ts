cac30a9e0e24dc7c0adcef1fcc33b13f53be1719
julianpoyourow <julian@poyourow.com>
2024-12-23 03:35:26 +0000

    feat: enable revision storage

--- libs/api-services/src/lib/artifacts/createArtifactRevision.ts
+++ libs/api-services/src/lib/artifacts/createArtifactRevision.ts
@@ -1,39 +1,59 @@
 import { prisma } from '@feynote/prisma/client';
 import { Prisma } from '@prisma/client';
 
+const REVISIONS_TO_KEEP = 10;
+
 export async function createArtifactRevision(
   artifactId: string,
   tx: Prisma.TransactionClient = prisma,
 ) {
-  const currentRevision = await tx.artifactRevision.findFirst({
+  const currentRevisionInfo = await tx.artifactRevision.aggregate({
     where: {
       artifactId,
     },
     orderBy: {
       revisionId: 'desc',
     },
-    select: {
+    _max: {
       revisionId: true,
-      artifactDeletedAt: true,
     },
   });
-  const revisionId = (currentRevision?.revisionId || 0) + 1;
+
+  const revisionId = (currentRevisionInfo._max.revisionId || 0) + 1;
 
   const artifact = await tx.artifact.findUniqueOrThrow({
     where: {
       id: artifactId,
     },
+    include: {
+      files: true,
+    },
   });
 
   // TODO: validate if user can create revisions/if we nuke an old revision depending on sub level
-  await tx.artifactRevision.create({
+  const createP = tx.artifactRevision.create({
     data: {
       artifactId: artifact.id,
       revisionId,
       userId: artifact.userId,
-      artifactJson: artifact as any, // TODO: artifact is actually incompatible because of yBin
-      artifactFilesJson: [],
-      artifactDeletedAt: currentRevision?.artifactDeletedAt || null,
+      artifactJson: {
+        ...artifact,
+        yBin: undefined,
+        yBinBase64: Buffer.from(artifact.yBin).toString('base64'),
+      },
+      artifactFilesJson: artifact.files,
+      artifactDeletedAt: null,
     },
   });
+
+  const deleteP = tx.artifactRevision.deleteMany({
+    where: {
+      artifactId,
+      revisionId: {
+        lt: revisionId - REVISIONS_TO_KEEP,
+      },
+    },
+  });
+
+  await Promise.all([createP, deleteP]);
 }
