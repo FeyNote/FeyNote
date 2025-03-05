463fc930e5bc67a88c921d0233adb524f3c1e891
julianpoyourow <julian@poyourow.com>
2025-03-05 00:31:15 +0000

    fix: add debounce and prevent out of order reference suggestions

--- libs/ui/src/components/editor/tiptap/extensions/artifactReferences/getReferenceSuggestions.ts
+++ libs/ui/src/components/editor/tiptap/extensions/artifactReferences/getReferenceSuggestions.ts
@@ -1,6 +1,13 @@
 import { trpc } from '../../../../../utils/trpc';
 import { ReferenceListItem } from './ReferenceListItem';
 
+const debounceMs = 150;
+/**
+ * Used to prevent parallel calls from overwiting each other out of order if network requests are resolved
+ * out of order in a fast typing situation.
+ */
+let lock: string | null = null;
+
 export const getReferenceSuggestions = (mentionMenuOptsRef: {
   componentRef: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any -- kill me, for I do not want to live anymore.
@@ -8,10 +15,16 @@ export const getReferenceSuggestions = (mentionMenuOptsRef: {
   };
 }) => {
   return async ({ query }: { query: string }): Promise<ReferenceListItem[]> => {
+    const myLock = crypto.randomUUID();
+    lock = myLock;
     mentionMenuOptsRef.componentRef.current?.updateProps({
       ...mentionMenuOptsRef.componentRef.current.props,
       searching: true,
     });
+
+    const debouncePromise = new Promise((resolve) =>
+      setTimeout(resolve, debounceMs),
+    );
 
     const artifactsPromise = trpc.artifact.searchArtifactTitles.query({
       query,
@@ -25,11 +38,14 @@ export const getReferenceSuggestions = (mentionMenuOptsRef: {
     const [artifacts, blocks] = await Promise.all([
       artifactsPromise,
       blocksPromise,
+      debouncePromise,
     ]).catch((e) => {
-      mentionMenuOptsRef.componentRef.current?.updateProps({
-        ...mentionMenuOptsRef.componentRef.current.props,
-        searching: false,
-      });
+      if (lock === myLock) {
+        mentionMenuOptsRef.componentRef.current?.updateProps({
+          ...mentionMenuOptsRef.componentRef.current.props,
+          searching: false,
+        });
+      }
 
       throw e;
     });
@@ -56,11 +72,15 @@ export const getReferenceSuggestions = (mentionMenuOptsRef: {
       });
     }
 
-    mentionMenuOptsRef.componentRef.current?.updateProps({
-      ...mentionMenuOptsRef.componentRef.current.props,
-      searching: false,
-    });
+    if (lock === myLock) {
+      mentionMenuOptsRef.componentRef.current?.updateProps({
+        ...mentionMenuOptsRef.componentRef.current.props,
+        searching: false,
+      });
 
-    return suggestionItems.slice(0, 20);
+      return suggestionItems.slice(0, 20);
+    }
+
+    return [];
   };
 };
