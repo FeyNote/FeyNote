c8ced3bcbe35816eaf9db1493791430c690cc443
julianpoyourow <julian@poyourow.com>
2025-04-20 05:43:32 +0000

    chore: add file check in s3 push script

--- scripts/push-frontend-s3.sh
+++ scripts/push-frontend-s3.sh
@@ -15,6 +15,8 @@ fi
 # We do not want to push .map files to S3 since they expose source code
 find www -type f -name "*.map" -delete
 
+[ -f "www/index.html" ] && [ -f "www/service-worker.js" ] || { echo "Both index.html and service-worker.js must exist. Something may have gone wrong during the build step"; exit 1; }
+
 aws s3 sync www s3://feynote-public/frontend/$TAG/ \
   --cache-control "public, max-age=900, must-revalidate"
 
