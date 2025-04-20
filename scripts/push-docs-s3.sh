35641588f1715c433c2012aeff6e86c3d8545096
julianpoyourow <julian@poyourow.com>
2025-04-20 05:56:18 +0000

    chore: add docs push fail check

--- scripts/push-docs-s3.sh
+++ scripts/push-docs-s3.sh
@@ -12,6 +12,8 @@ then
   exit 1
 fi
 
+[ -f "apps/docs/dist/index.html" ] || { echo "index.html must exist. Something may have gone wrong during the build step"; exit 1; }
+
 aws s3 sync apps/docs/dist s3://feynote-public/docs/$TAG/ \
   --cache-control "public, max-age=3600, must-revalidate"
 
