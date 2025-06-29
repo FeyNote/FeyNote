34dbf0d805e9ede05827e237f48f3e2d5257d19d
cmeyer <chrismeyer175@gmail.com>
2025-06-29 23:04:35 +0000

    removing meta override

--- libs/queue/src/lib/jobQueue/import/writeStandardizedImport.ts
+++ libs/queue/src/lib/jobQueue/import/writeStandardizedImport.ts
@@ -31,12 +31,20 @@ export const writeStandardizedImport = async (args: {
     const artifactIds = createdArtifacts.map(
       (createdArtifact) => createdArtifact.id,
     );
+
+    const existingJob = await prisma.job.findUnique({
+      where: { id: args.job.id },
+      select: { meta: true },
+    });
+
+    const meta = {
+      ...(existingJob?.meta as JobSummary['meta']),
+      createdArtifacts: artifactIds,
+    };
     await tx.job.update({
       where: { id: args.job.id },
       data: {
-        meta: {
-          createdArtifacts: artifactIds,
-        },
+        meta,
       },
     });
 
