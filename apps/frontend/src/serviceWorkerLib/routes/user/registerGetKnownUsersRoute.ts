8a8485830677dd44f3652c11b2043aa8b515c879
julianpoyourow <julian@poyourow.com>
2025-09-28 17:16:14 +0000

    fix: add compat for new typed cacheListResponse to getKnownUsers cache resolver

--- apps/frontend/src/serviceWorkerLib/routes/user/registerGetKnownUsersRoute.ts
+++ apps/frontend/src/serviceWorkerLib/routes/user/registerGetKnownUsersRoute.ts
@@ -1,12 +1,16 @@
 import { registerRoute } from 'workbox-routing';
 import { cacheListResponse } from '../../util/cacheListResponse';
-import { ObjectStoreName } from '@feynote/ui-sw';
+import { ObjectStoreName, trpc } from '@feynote/ui-sw';
 
 export function registerGetKnownUsersRoute() {
   registerRoute(
     /((https:\/\/api\.feynote\.com)|(\/api))\/trpc\/user\.getKnownUsers/,
     async (event) => {
-      return cacheListResponse(ObjectStoreName.KnownUsers, event);
+      return cacheListResponse(
+        ObjectStoreName.KnownUsers,
+        event,
+        trpc.user.getKnownUsers.query,
+      );
     },
     'GET',
   );
