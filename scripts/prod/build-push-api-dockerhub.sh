4dc611947c0970d15dc47d156b8fce9693887649
julianpoyourow <julian@poyourow.com>
2026-01-03 02:44:36 +0000

    feat: build to feynote dockerhub

--- scripts/prod/build-push-api-dockerhub.sh
+++ scripts/prod/build-push-api-dockerhub.sh
@@ -8,18 +8,18 @@ then
   exit 1
 fi
 
-docker build -f Dockerfile.production --build-arg APP_VERSION=$1 -t redchickenco/feynote:api-latest .
+docker build -f Dockerfile.production --build-arg APP_VERSION=$1 -t feynote/feynote:api-latest .
 
 # Only push to latest tag if tag is a versioned tag
 if [[ $1 == v* ]]
 then
-  docker push redchickenco/feynote:api-latest
+  docker push feynote/feynote:api-latest
 fi
 
-docker image tag redchickenco/feynote:api-latest redchickenco/feynote:api-$1
-docker push redchickenco/feynote:api-$1
+docker image tag feynote/feynote:api-latest feynote/feynote:api-$1
+docker push feynote/feynote:api-$1
 
 # Cleanup
-docker rmi redchickenco/feynote:api-$1
-docker rmi redchickenco/feynote:api-latest
+docker rmi feynote/feynote:api-$1
+docker rmi feynote/feynote:api-latest
 
