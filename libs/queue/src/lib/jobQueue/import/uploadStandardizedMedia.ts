ea051e14d8955d96208799296ed7417639cdf1f8
Chris Meyer <chrismeyer175@gmail.com>
2026-06-07 15:26:27 -0700

    fix: media upload timeout and error handling (#2021)

--- libs/queue/src/lib/jobQueue/import/uploadStandardizedMedia.ts
+++ libs/queue/src/lib/jobQueue/import/uploadStandardizedMedia.ts
@@ -1,6 +1,7 @@
 import {
   logger,
   proxyGetRequest,
+  TimeoutError,
   transformAndUploadFileToS3ForUser,
 } from '@feynote/api-services';
 import type { StandardizedImportInfo } from './StandardizedImportInfo';
@@ -14,82 +15,118 @@ import type { JobProgressTracker } from '../JobProgressTracker';
 
 const ALLOWED_NUMBER_OF_HTTP_LINKS_PER_UPLOAD = 5000;
 const UPLOAD_CONCURRENCY = 3;
+const MEDIA_PROCESSING_REQUEST_TIMEOUT = 15000;
 
-const limit = pLimit(UPLOAD_CONCURRENCY);
+class TooManyLinksError extends Error {
+  constructor() {
+    super();
+    this.name = 'TooManyLinksError';
+  }
+}
 
 export const uploadStandardizedMedia = async (
   userId: string,
   importInfo: StandardizedImportInfo,
   progressTracker: JobProgressTracker,
 ) => {
+  const limit = pLimit(UPLOAD_CONCURRENCY);
   let counter = 0;
   const results = await Promise.allSettled(
     importInfo.mediaFilesToUpload.map(async (mediaInfo, idx) => {
       return limit(async () => {
-        counter++;
+        try {
+          counter++;
 
-        if (counter > ALLOWED_NUMBER_OF_HTTP_LINKS_PER_UPLOAD) {
-          throw new Error('Too many http links');
-        }
+          if (counter > ALLOWED_NUMBER_OF_HTTP_LINKS_PER_UPLOAD) {
+            throw new TooManyLinksError();
+          }
 
-        let fileName: string;
-        let ext: string;
-        let file = new Readable();
-        if ('url' in mediaInfo) {
-          logger.debug(`Retrieving media content from ${mediaInfo.url}`);
-          const response = await proxyGetRequest({
-            url: mediaInfo.url,
-            config: {
-              responseType: 'stream',
-              signal: AbortSignal.timeout(15000),
-            },
+          let fileName: string;
+          let ext: string;
+          let file = new Readable();
+
+          const abortController = new AbortController();
+          const timeout = setTimeout(() => {
+            abortController.abort(new TimeoutError());
+            file.destroy();
+          }, MEDIA_PROCESSING_REQUEST_TIMEOUT);
+
+          if ('url' in mediaInfo) {
+            logger.debug(`Retrieving media content from ${mediaInfo.url}`);
+            const response = await proxyGetRequest({
+              url: mediaInfo.url,
+              config: {
+                responseType: 'stream',
+                signal: abortController.signal,
+              },
+            });
+
+            file = response.data;
+            ext = extname(mediaInfo.url);
+            fileName = basename(mediaInfo.url, ext);
+          } else {
+            logger.debug(
+              `Beinning stream of local stored media content ${mediaInfo.path}`,
+            );
+            ext = extname(mediaInfo.path);
+            fileName = basename(mediaInfo.path, ext);
+            file = createReadStream(mediaInfo.path);
+          }
+
+          const purpose = FilePurpose.artifact;
+          const mimetype = mime.lookup(ext);
+
+          const { uploadResult } = await transformAndUploadFileToS3ForUser({
+            userId,
+            file,
+            purpose,
+            mimetype,
+            storageKey: mediaInfo.storageKey,
           });
 
-          file = response.data;
-          ext = extname(mediaInfo.url);
-          fileName = basename(mediaInfo.url, ext);
-        } else {
-          logger.debug(
-            `Beinning stream of local stored media content ${mediaInfo.path}`,
-          );
-          ext = extname(mediaInfo.path);
-          fileName = basename(mediaInfo.path, ext);
-          file = createReadStream(mediaInfo.path);
+          if (abortController.signal.aborted) {
+            throw new TimeoutError();
+          }
+          clearTimeout(timeout);
+
+          const fileData = {
+            id: mediaInfo.id,
+            artifactId: mediaInfo.associatedArtifactId,
+            userId,
+            name: fileName,
+            mimetype,
+            storageKey: uploadResult.key,
+            purpose,
+            metadata: {
+              uploadResult,
+            },
+          };
+
+          progressTracker.onProgress({
+            progress: Math.floor(
+              (idx / importInfo.mediaFilesToUpload.length) * 100,
+            ),
+            step: 2,
+          });
+
+          logger.debug(`Finished uploading media ${fileName}`);
+          return fileData;
+        } catch (e) {
+          if (e instanceof TimeoutError) {
+            logger.debug(
+              `Media processing timeout hit while processing data from url ${'url' in mediaInfo ? mediaInfo.url : mediaInfo.path}`,
+            );
+            throw e;
+          }
+          if (e instanceof TooManyLinksError) {
+            logger.debug(
+              `Too many http links in import job, skipping media upload`,
+            );
+            throw e;
+          }
+          logger.error(e);
+          throw e;
         }
-
-        const purpose = FilePurpose.artifact;
-        const mimetype = mime.lookup(ext);
-
-        const { uploadResult } = await transformAndUploadFileToS3ForUser({
-          userId,
-          file,
-          purpose,
-          mimetype,
-          storageKey: mediaInfo.storageKey,
-        });
-
-        const fileData = {
-          id: mediaInfo.id,
-          artifactId: mediaInfo.associatedArtifactId,
-          userId,
-          name: fileName,
-          mimetype,
-          storageKey: uploadResult.key,
-          purpose,
-          metadata: {
-            uploadResult,
-          },
-        };
-
-        progressTracker.onProgress({
-          progress: Math.floor(
-            (idx / importInfo.mediaFilesToUpload.length) * 100,
-          ),
-          step: 2,
-        });
-
-        logger.debug(`Finished uploading media ${fileName}`);
-        return fileData;
       });
     }),
   );
